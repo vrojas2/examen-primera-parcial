7ce656f630fdafaef42b32fefdb59ac866a6d822
20156636 ROJAS BAYARDO VICTOR <20156636@TELEMATICA.UCOL.MX>
2019-09-18 15:57:42 -0500

    numeros amigos hasta 3000

--- numAmigos.js
+++ numAmigos.js
@@ -1,21 +1,23 @@
-var ri, rj;
+let btn = document.querySelector("#mostrar").addEventListener("click", () => {
+    var ri, rj;
+    function dividir(x) {
+        let sumaNum = 0;
+        for (let k = 1; k <= Math.floor(x / 2) + 1; k++) {
+            if (x % k == 0) {
+                sumaNum = sumaNum + k;
+            }
+        }
+        return sumaNum;
+    }
 
-function dividir(x) {
-    let sumaNum = 0;
-    for (let k = 1; k <= Math.floor(x / 2) + 1; k++) {
-        if (x % k == 0) {
-            sumaNum = sumaNum + k;
+    for (let i = 2; i <= 3000; i++) {
+        ri = dividir(i);
+        for (let j = i; j <= 3000; j++) {
+            rj = dividir(j);
+            if (ri == j && rj == i) {
+                console.log(i, "y", j, "son amigos");
+            }
         }
     }
-    return sumaNum;
-}
+});
 
-for (let i = 2; i <= 3000; i++) {
-    ri = dividir(i);
-    for (let j = i; j <= 3000; j++) {
-        rj = dividir(j);
-        if (ri == j && rj == i) {
-            console.log(i, "y", j, "son amigos");
-        }
-    }
-}
