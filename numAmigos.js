f364ded13cb12c074893c5c6f9bd3442c59e25b6
20156636 ROJAS BAYARDO VICTOR <20156636@TELEMATICA.UCOL.MX>
2019-09-18 15:43:11 -0500

    numeros amigos

---
+++ numAmigos.js
@@ -0,0 +1,21 @@
+var ri, rj;
+
+function dividir(x) {
+    let sumaNum = 0;
+    for (let k = 1; k <= Math.floor(x / 2) + 1; k++) {
+        if (x % k == 0) {
+            sumaNum = sumaNum + k;
+        }
+    }
+    return sumaNum;
+}
+
+for (let i = 2; i <= 300; i++) {
+    ri = dividir(i);
+    for (let j = i; j <= 300; j++) {
+        rj = dividir(j);
+        if (ri == j && rj == i) {
+            console.log(i, "y", j, "son amigos");
+        }
+    }
+}
